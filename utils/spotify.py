17ac27015ac169cfa2ab585e80f9fcac7a903d95
roddajohn <johnrod.john@gmail.com>
2017-01-10 14:04:13 -0500

    Got Spotify authentication working

--- utils/spotify.py
+++ utils/spotify.py
@@ -1,26 +1,37 @@
 # Greetings, written by Rodda
 
 import requests
-import urllib
+import urllib, urllib2
+import base64
+import json
 
 # search_field should be a string of the search field
 # type should be either album, artist, playlist, or track
 def search(search_field = '', type='artist'):
     query_request = {'q' : search_field, 'type' : type}
     encoded = urllib.urlencode(query_request)
-    url = 'https://api.spotify.com/v1/search?' + encoded
+    
+    url = 'https://accounts.spotify.com/api/token'
 
-#    keys = 'Basic ' + open('spotify_key').read().split('\n')[0] + ':' + open('spotify_key').read().split('\n')[1]
-#    print keys
-#    headers = {'Authorization' : keys}
+    CLIENT_ID = open('spotify_key').read().split('\n')[0]
+    CLIENT_SECRET = open('spotify_key').read().split('\n')[1]
 
-#    r = requests.get(url, headers = headers)
+    base64encoded = base64.b64encode("{}:{}".format(CLIENT_ID, CLIENT_SECRET))
+    headers = {"Authorization": "Basic {}".format(base64encoded)}
 
-    r = requests.get(url)
+    payload = {
+        'grant_type': 'client_credentials'
+    }
 
-    print r
-    print r.headers
-    print r.text
+    data_encoded = urllib.urlencode(payload)
+    
+    r = urllib2.Request(url, data_encoded, headers)
+
+    response = urllib2.urlopen(r, timeout = 30).read()
+    response_data = json.loads(response)
+    
+    print response_data
+
 
 search('hi')
     
