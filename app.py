0cc5e8cd20cfbf6ca1dc5136f2898025acfffbf2
Jason Mohabir <jmohabir@stuy.edu>
2017-01-10 13:35:59 -0500

    Adding app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-import sqlite3
-import utils.master
-import utils.ticketmaster
-import utils.ticketleap
-import utils.seatgeek
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    return render_template("index.html")
+        return render_template("index.html")
 
-@app.route("/output", methods=["POST"])
-def output():
-    return render_template("output.html",events=eventList)
+
+@app.route("/loginRegister")
+def notLoggedIn():
+        return render_template("notLoggedIn.html")
+
+@app.route("/map")
+def mapTester():
+        return render_template("mapTester.html")
 
 if __name__ == "__main__":
-    app.debug = False
+    #app.debug = True                                                                                                        
     app.run()
