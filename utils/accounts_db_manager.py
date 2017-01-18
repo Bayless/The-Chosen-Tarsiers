d8b52be280aaf66fff9aad53bfd724c600ace843
Jason Mohabir <jmohabir@stuy.edu>
2017-01-18 11:04:38 -0500

    Clean

---
+++ utils/accounts_db_manager.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+def new_access_token(song, user):
+    f = 'database.db'
+    db = sqlite3.connect(f)
+
+    c = db.cursor()
+
+    new_access_token = 'INSERT INTO account VALUES ("%s", %d);' % (access_token, time)
+    c.execute(new_access_token)
+
+    db.commit()
+    db.close()
+
