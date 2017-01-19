cc4bbef634f15e0435266dc535b145f3925f87ac
Bayless <bsmithsalzberg@stuy.edu>
2017-01-19 18:47:16 -0500

    here ya are

--- utils/accounts_db_manager.py
+++ utils/accounts_db_manager.py
@@ -1,12 +1,12 @@
 import sqlite3
 
-def new_access_token(song, user):
+def new_access_token(user,track,artist):
     f = 'database.db'
     db = sqlite3.connect(f)
 
     c = db.cursor()
 
-    new_access_token = 'INSERT INTO songs VALUES ("%s", %d);' % (access_token, time)
+    new_access_token = 'INSERT INTO songs VALUES ("%s", "%s");' % (username, track, artist)
     c.execute(new_access_token)
 
     db.commit()
